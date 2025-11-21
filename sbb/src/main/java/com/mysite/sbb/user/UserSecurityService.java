8740bf3c958fb89a611c40da55168cf5d77e3b11
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2025-11-21 10:23:55 +0900

    usersecurityservice.. commit

---
+++ sbb/src/main/java/com/mysite/sbb/user/UserSecurityService.java
@@ -0,0 +1,35 @@
+package com.mysite.sbb.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        Optional<SiteUser> _siteUser = this.userRepository.findByUserId(userId);
+        if (_siteUser.isEmpty()) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+        SiteUser siteUser = _siteUser.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new User(siteUser.getUserId(), siteUser.getPassword(), authorities);
+    }
+}
