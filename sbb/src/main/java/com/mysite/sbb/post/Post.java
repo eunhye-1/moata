2a438b4e4ea18746eb9dbf96e177bdc96a8054e0
HwOdust <todka05@gmail.com>
2025-11-21 13:17:31 +0900

    커뮤니티 유저&글 연동

--- sbb/src/main/java/com/mysite/sbb/post/Post.java
+++ sbb/src/main/java/com/mysite/sbb/post/Post.java
@@ -9,10 +9,10 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-// import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.*;
 
-// import com.mysite.sbb.user.SiteUser; // 유저 엔티티 가져오기
+import com.mysite.sbb.user.SiteUser; // 유저 엔티티 가져오기
 
 @Getter
 @Setter
@@ -21,8 +21,11 @@ public class Post{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+	
+	@ManyToOne //fk 걔 ㅇㅇ
+	private SiteUser user;
 
-    @Column(length=10)
+    @Column(length=100)
     private String category; 
 
     @Column(length = 100, nullable = false)
